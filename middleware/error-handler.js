9dfb57a2b2397dac70638191232f3fc22e1443c4
Mikey Nichols <mnix@journeytocode.io>
2026-04-24 21:22:32 -0400

    updates error-handler to respond with json

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -2,7 +2,7 @@ const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
   console.error(
-    "Internal server error:",
+    "Internal server error: ",
     err.constructor.name,
     JSON.stringify(err, ["name", "message", "stack"]),
   );
@@ -10,7 +10,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   if (!res.headersSent) {
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .send("An internal server error occurred.");
+      .json({ message: "An internal server error occurred." });
   }
 };
 
