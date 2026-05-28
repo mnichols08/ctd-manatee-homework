bb27057dda777ad69e957f8b794d8bee70938c7b
Mikey Nichols <mnix@journeytocode.io>
2026-05-28 14:37:18 -0400

    add JWT middleware to logoff route for enhanced security

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { logon, register, logoff } = require("../controllers/userController");
+const jwtMiddleware = require("../middleware/jwtMiddleware");
 
 router.post("/register", register);
 router.post("/logon", logon);
-router.post("/logoff", logoff);
+router.post("/logoff", jwtMiddleware, logoff);
 
 module.exports = router;
